c3e4e81e51ec6a08b92070b7f810506d395bb54c
Lavanya Rapelli <lavanyarapelli555@gmail.com>
2024-08-04 16:11:48 +0530

    patterns

--- patterns.js
+++ patterns.js
@@ -1,14 +1,28 @@
 // Pttern U
-function printU(n) {
-    for (let i = 0; i < n; i++) {
-        let row = '';
-        for (let j = 0; j < n; j++) {
-            if (i === n - 1 || j === 0 || j === n - 1) {
-                row += '*';
-            } else {
-                row += ' ';
-            }
-        }
-        console.log(row);
-    }
-}
+// function printU(n) {
+//     for (let i = 0; i < n; i++) {
+//         let row = '';
+//         for (let j = 0; j < n; j++) {
+//             if (i === n - 1 || j === 0 || j === n - 1) {
+//                 row += '*';
+//             } else {
+//                 row += ' ';
+//             }
+//         }
+//         console.log(row);
+//     }
+// }
+
+// function printN(n) {
+//     for (let i = 0; i < n; i++) {
+//         let row = '';
+//         for (let j = 0; j < n; j++) {
+//             if (j === 0 || j === n - 1 || j === i) {
+//                 row += '*';
+//             } else {
+//                 row += ' ';
+//             }
+//         }
+//         console.log(row);
+//     }
+// }
